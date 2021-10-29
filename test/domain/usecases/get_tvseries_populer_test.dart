c6736089e71c6a7127675e8b38250c5252e47847
Rifat Khadafy <khadafy67@gmail.com>
2021-10-29 14:43:02 +0700

    add test coverage

--- test/domain/usecases/get_tvseries_populer_test.dart
+++ test/domain/usecases/get_tvseries_populer_test.dart
@@ -1,1 +1,33 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/entities/tv_series.dart';
+import 'package:ditonton/domain/usecases/get_populer_tvseries.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late GetPopulerTvSeries usecase;
+  late MockTvSeriesRepository mockTvSeriesRepository;
+
+  setUp(() {
+    mockTvSeriesRepository = MockTvSeriesRepository();
+    usecase = GetPopulerTvSeries(mockTvSeriesRepository);
+  });
+
+  final tSeries = <TvSeries>[];
+
+  group('GetPopularMovies Tests', () {
+    test(
+        'should get list of series from the repository when execute function is called',
+        () async {
+      // arrange
+      when(mockTvSeriesRepository.getPopularTvSeries())
+          .thenAnswer((_) async => Right(tSeries));
+      // act
+      final result = await usecase.execute();
+      // assert
+      expect(result, Right(tSeries));
+    });
+  });
+}
